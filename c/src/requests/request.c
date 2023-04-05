2bfa3364564da83e6ad7427c6f9194be53bf0680
Bruno Genero <generobruno@gmail.com>
2023-04-04 21:42:19 -0300

    getting data

--- c/src/requests/request.c
+++ c/src/requests/request.c
@@ -1,27 +1,102 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include <curl/curl.h>
 
 #include "../../inc/cJSON/cJSON.h"
+struct MemoryStruct {
+  char *memory;
+  size_t size;
+};
+ 
+static size_t WriteMemoryCallback(void *contents, size_t size, size_t nmemb, void *userp) {
+  size_t realsize = size * nmemb;
+  struct MemoryStruct *mem = (struct MemoryStruct *)userp;
+ 
+  char *ptr = realloc(mem->memory, mem->size + realsize + 1);
+  if(!ptr) {
+    /* out of memory! */
+    printf("not enough memory (realloc returned NULL)\n");
+    return 0;
+  }
+ 
+  mem->memory = ptr;
+  memcpy(&(mem->memory[mem->size]), contents, realsize);
+  mem->size += realsize;
+  mem->memory[mem->size] = 0;
+ 
+  return realsize;
+}
  
 int main(void) {
-  CURL *curl;
+  CURL *curl_handle;
   CURLcode res;
  
-  curl = curl_easy_init();
-  if(curl) {
-    curl_easy_setopt(curl, CURLOPT_URL, "https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency=BTC&to_currency=USD&apikey=EG7JWLOAMEKIU3IR");
-    /* example.com is redirected, so we tell libcurl to follow redirection */
-    curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1L);
+  struct MemoryStruct chunk;
  
-    /* Perform the request, res will get the return code */
-    res = curl_easy_perform(curl);
-    /* Check for errors */
-    if(res != CURLE_OK)
-      fprintf(stderr, "curl_easy_perform() failed: %s\n",
-              curl_easy_strerror(res));
+  chunk.memory = malloc(1);  /* will be grown as needed by the realloc above */
+  chunk.size = 0;    /* no data at this point */
  
-    /* always cleanup */
-    curl_easy_cleanup(curl);
+  curl_global_init(CURL_GLOBAL_ALL);
+ 
+  /* init the curl session */
+  curl_handle = curl_easy_init();
+ 
+  /* specify URL to get */
+  curl_easy_setopt(curl_handle, CURLOPT_URL, "https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency=BTC&to_currency=USD&apikey=EG7JWLOAMEKIU3IR");
+ 
+  /* send all data to this function  */
+  curl_easy_setopt(curl_handle, CURLOPT_WRITEFUNCTION, WriteMemoryCallback);
+ 
+  /* we pass our 'chunk' struct to the callback function */
+  curl_easy_setopt(curl_handle, CURLOPT_WRITEDATA, (void *)&chunk);
+ 
+  /* some servers do not like requests that are made without a user-agent
+     field, so we provide one */
+  curl_easy_setopt(curl_handle, CURLOPT_USERAGENT, "libcurl-agent/1.0");
+ 
+  /* get it! */
+  res = curl_easy_perform(curl_handle);
+ 
+  /* check for errors */
+  if(res != CURLE_OK) {
+    fprintf(stderr, "curl_easy_perform() failed: %s\n",
+            curl_easy_strerror(res));
   }
+  else {
+    /*
+     * Now, our chunk.memory points to a memory block that is chunk.size
+     * bytes big and contains the remote file.
+     *
+     * Do something nice with it!
+     */
+ 
+    printf("%lu bytes retrieved\n", (unsigned long)chunk.size);
+
+
+    printf("DATA: %s\n", (char *)chunk.memory);
+  }
+
+  // TODO Parsear JSON
+  cJSON *jason = cJSON_Parse(chunk.memory);
+  const cJSON *data = cJSON_GetObjectItemCaseSensitive(jason, "Realtime Currency Exchange Rate");
+  cJSON *info = NULL;
+  cJSON_ArrayForEach(info, data) {
+    cJSON *rate = cJSON_GetObjectItemCaseSensitive(info, "5. Exchange Rate");
+    if(cJSON_IsNumber(rate)) {
+      printf("RATE: %f\n", rate->valuedouble);
+    }
+  }
+
+  cJSON_Delete(jason);
+ 
+  /* cleanup curl stuff */
+  curl_easy_cleanup(curl_handle);
+ 
+  free(chunk.memory);
+ 
+  /* we are done with libcurl, so clean it up */
+  curl_global_cleanup();
+ 
   return 0;
 }
